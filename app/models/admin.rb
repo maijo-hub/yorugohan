2b159fa63a8e00b9fb6f51bde0d2316fe3dad8aa
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-07-03 07:08:18 +0000

    [Start管理者ログイン]

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,8 @@
-class User < ApplicationRecord
+class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+
+  devise authentication_keys: [:email]
 end
