97dca0fd8893aa301fa2812382fedf55b912b8ab
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-07-08 07:04:12 +0000

    [finish]ユーザー編集削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top], unless: :admin_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,5 +12,18 @@ class ApplicationController < ActionController::Base
     dinners_path
   end
 
-end
+  def after_sign_in_path_for(resource)
+    dinners_path
+  end
 
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile])
+  end
+end
