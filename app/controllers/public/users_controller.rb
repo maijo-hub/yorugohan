955edb446d766439c6178bee962b846f2dcfb1a8
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-07-08 02:48:26 +0000

    [add]ユーザー編集削除

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,38 @@
+class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :confirm_withdrawal, :withdraw]
+
+  def show
+    @dinners = @user.dinners.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "プロフィールを更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def confirm_withdrawal
+  end
+
+  def withdraw
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path, notice: "退会が完了しました。ご利用ありがとうございました。"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :profile)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,34 @@
 Rails.application.routes.draw do
-  
-    devise_for :admin, skip: [:registrations, :password], controllers: {
+
+  # 管理者
+  devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
 
+  # 一般ユーザー
   scope module: :public do
     devise_for :users
     root to: 'dinners#top'
+
+    # 投稿関連
     resources :post_images, only: [:new, :create, :index, :show, :destroy] do
       resource :favorites, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
     end
 
-    resources :dinners, only: [:index, :show, :new, :create, :edit, :update, :destroy]    
+    # 夜ごはん
+    resources :dinners, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
-    resources :users, only: [:show, :edit, :update]
+    # ユーザー関連
+    resources :users, only: [:show, :edit, :update] do
+      member do
+        get 'confirm_withdrawal'
+        patch 'withdraw'
+      end
+    end
   end
 
+  # 管理者用
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:destroy]
